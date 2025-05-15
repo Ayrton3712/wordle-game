a8d3396fb90aeffe6c50089a1358a71468a4cf1f
Ayrton3712 <mugittty@gmail.com>
2025-05-15 13:31:16 -0300

    Added raw input_game_loop.cpp, created display.h, modified word_comparison.h

---
+++ include/display.h
@@ -0,0 +1,10 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+class Display{
+    public:
+        void displayFeedback(const vector<int>&, const string&);
+        void displayGameState();
+};
+
+#endif
---
+++ include/word_comparison.h
@@ -0,0 +1,9 @@
+#ifndef WORD_COMPARISON_H
+#define WORD_COMPARISON_H
+
+class WordComparison{
+    public:
+        vector<int> evaluateGuess(const string&);
+};
+
+#endif
