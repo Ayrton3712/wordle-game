6bf8d7fe7ca07a198213c6f79f3786c524a812e4
edna <edna.irala>
2025-05-15 16:07:33 -0300

    library set up and prototype functions

---
+++ src/display.cpp
@@ -0,0 +1,7 @@
+//Edna:  Display & Output
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void displayFeedback(const vector<int>& feedback, const string& guess);
+void displayGameState();
